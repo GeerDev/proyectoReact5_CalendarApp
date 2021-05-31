69a257c6979482002b9f1934941bf7f4e9ece36c
German <gerfersie@gmail.com>
2021-05-31 14:53:47 +0200

    Testing Calendar App

---
+++ src/tests/helpers/fetch.test.js
@@ -0,0 +1,31 @@
+import { fetchSinToken, fetchConToken } from '../../helpers/fetch';
+
+describe('Pruebas en el helper Fetch', () => {
+
+    let token = '';
+
+    test('fetchSinToken debe de funcionar', async() => {
+
+        const resp = await fetchSinToken('auth', { email: 'gerfersie@gmail.com', password: '123456' }, 'POST');
+
+        expect( resp instanceof Response ).toBe( true );
+        console.log(resp)
+        const body = await resp.json();
+        expect( body.ok ).toBe( true );
+
+        token = body.token;
+        
+    })
+
+    test('fetchConToken debe de funcionar', async() => {
+
+        localStorage.setItem('token', token );
+
+        const resp = await fetchConToken('events/5ee25d21c25cce32af01a3f3', {}, 'DELETE');
+        const body = await resp.json();
+
+        expect( body.msg ).toBe('Evento no existe por ese id');
+        
+    })
+    
+})
